7eca1db2bff3a2144939ad997448f8c6dec37d3d
Italo Gabriel <italo.pereira@pagtel.com.br>
2022-12-15 09:54:05 -0300

    test: ensure FormatString methods

---
+++ test/unit/utils/format-string.spec.ts
@@ -0,0 +1,45 @@
+import { FormatString } from "@/utils";
+
+describe("#FormatString", () => {
+  test("#convertToKebabCase should convert a string to kebabCase", () => {
+    const data = "HelloMyWord";
+    const expected = "hello-my-word";
+    const result = FormatString.convertToKebabCase(data);
+    expect(result).toEqual(expected);
+  });
+
+  test("#upperCaseFirstLetter should transform the first letter in upperCase", () => {
+    const data = "hello";
+    const expected = "Hello";
+    const result = FormatString.upperCaseFirstLetter(data);
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("#lowerCaseFirstLetter should transform the first letter in lowerCase", () => {
+    const data = "Hello";
+    const expected = "hello";
+    const result = FormatString.lowerCaseFirstLetter(data);
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("#convertToKebabCase given an empty string it should return empty", () => {
+    const data = "";
+    const expected = "";
+    const result = FormatString.convertToKebabCase(data);
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("#lowerCaseFirstLetter given an empty string it should return empty", () => {
+    const data = "";
+    const expected = "";
+    const result = FormatString.lowerCaseFirstLetter(data);
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("#upperCaseFirstLetter given an empty string it should return empty", () => {
+    const data = "";
+    const expected = "";
+    const result = FormatString.upperCaseFirstLetter(data);
+    expect(result).toStrictEqual(expected);
+  });
+});
