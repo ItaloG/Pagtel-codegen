fc7fa0ca71967b9faaebafe991e6286a52532ab7
Italo Gabriel <italo.pereira@pagtel.com.br>
2022-12-15 09:53:45 -0300

    feat: add FormatString util class

--- src/utils/format-string.ts
+++ src/utils/format-string.ts
@@ -1,11 +1,35 @@
 export class FormatString {
-  static convertToKebabCase({
+  protected static transform({
     string,
-  }: ConvertToKebabCase.Params): ConvertToKebabCase.Result {}
+    upperCase = true,
+  }: Transform.Params): Transform.Result {
+    const [first, ...rest] = string;
+
+    if (!first) return "";
+
+    const firstLetter = upperCase ? first.toUpperCase() : first.toLowerCase();
+
+    return [firstLetter, ...rest].join("");
+  }
+
+  static convertToKebabCase(string: string): string {
+    const formattedFirstLetter = this.transform({ string, upperCase: false });
+    return formattedFirstLetter.replace(/(?=[A-Z])/g, "-").toLowerCase();
+  }
+
+  static upperCaseFirstLetter(string: string) {
+    return this.transform({ string });
+  }
+
+  static lowerCaseFirstLetter(string: string) {
+    return this.transform({ string, upperCase: false });
+  }
 }
 
-export namespace ConvertToKebabCase {
-  export type Params = { string: string };
-
+namespace Transform {
+  export type Params = {
+    string: string;
+    upperCase?: boolean;
+  };
   export type Result = string;
 }
