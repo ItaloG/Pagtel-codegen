50f3fb8b10025d719ff26a37cf5cbc3cc55bcdfd
Italo Gabriel <italo.pereira@pagtel.com.br>
2022-12-16 16:18:41 -0300

    chore: update project configs

--- jest.config.ts
+++ jest.config.ts
@@ -26,7 +26,7 @@ export default {
   collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
 
   // The directory where Jest should output its coverage files
-  // coverageDirectory: undefined,
+  coverageDirectory: "coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
@@ -147,7 +147,7 @@ export default {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  // testEnvironment: "jest-environment-node",
+  testEnvironment: "node",
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
