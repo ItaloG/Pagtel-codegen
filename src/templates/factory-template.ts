618cd1ed033e2a0b3f85b6fc1869a493c653bd21
Italo Gabriel <italo.pereira@pagtel.com.br>
2022-12-16 16:18:08 -0300

    feat: add factory generator function

--- src/templates/factory-template.ts
+++ src/templates/factory-template.ts
@@ -1,8 +1,52 @@
-import { GetExampleMiddleware } from '@/presentation/middlewares';
+import { FormatString } from "@/utils";
 
-import { makeErrorHandler } from "../../usecases";
+const componentNameAnchor = "$$componentName";
 
-export const makeGetExampleMiddleware = () => {
-  const usecase = {};
-  return new GetExampleMiddleware(usecase, makeErrorHandler());
+const FACTORY_TEMPLATES = {
+  middleware: `
+import { $$componentNameMiddleware } from '@/presentation/middlewares';
+
+import { makeErrorHandler } from '../../usecases';
+
+export const make$$componentNameMiddleware = () => {
+  return new $$componentNameMiddleware(, makeErrorHandler());
+};`,
+  controller: `
+import { CreateExampleController } from '@/presentation/controllers';
+import { commitAll } from '@/util';
+
+export const makeCreateExampleController = () =>
+  new CreateExampleController(commitAll);`,
 };
+
+const FACTORY_TYPES = ["middleware", "controller"];
+
+export function generateFactoryTemplate({
+  componentName,
+  componentType,
+}: FactoryTemplate.Params): FactoryTemplate.Result {
+  if (!FACTORY_TYPES.includes(componentType))
+    throw new Error("Invalid component type");
+
+  const template = FACTORY_TEMPLATES[componentType];
+
+  const templateFill = template.replaceAll(
+    componentNameAnchor,
+    FormatString.upperCaseFirstLetter(componentName)
+  );
+
+  return {
+    template: templateFill,
+  };
+}
+
+namespace FactoryTemplate {
+  export type Params = {
+    componentName: string;
+    componentType: "middleware" | "controller";
+  };
+
+  export type Result = {
+    template: string;
+  };
+}
