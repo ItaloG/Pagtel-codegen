b2a355e08e65e63798b4f7b06a416f3fd5d224fd
Italo Gabriel <italog03@gmail.com>
2022-12-18 10:57:47 -0300

    refactor: change tests description

--- test/unit/templates.spec.ts
+++ test/unit/templates.spec.ts
@@ -7,38 +7,40 @@ import {
 describe("#Template Generator", () => {
   const componentName = "example";
 
-  test("#generateFactoryTemplate should generate middleware factory template", () => {
-    const expected = {
-      template: middlewareFactoryTemplateMock,
-    };
+  describe("#generateFactoryTemplate", () => {
+    it("should generate middleware factory template", () => {
+      const expected = {
+        template: middlewareFactoryTemplateMock,
+      };
 
-    const result = generateFactoryTemplate({
-      componentName,
-      componentType: "middleware",
-    });
-    expect(result).toStrictEqual(expected);
-  });
-
-  test("#generateFactoryTemplate should generate controller factory template", () => {
-    const expected = {
-      template: controllerFactoryTemplateMock,
-    };
-
-    const result = generateFactoryTemplate({
-      componentName,
-      componentType: "controller",
-    });
-    expect(result).toStrictEqual(expected);
-  });
-
-  test("#generateFactoryTemplate should throw a error if a invalid componentType has been passed", () => {
-    const expected = "Invalid component type";
-
-    expect(() =>
-      generateFactoryTemplate({
+      const result = generateFactoryTemplate({
         componentName,
-        componentType: <any>"test",
-      })
-    ).toThrowError(expected);
+        componentType: "middleware",
+      });
+      expect(result).toStrictEqual(expected);
+    });
+
+    it("should generate controller factory template", () => {
+      const expected = {
+        template: controllerFactoryTemplateMock,
+      };
+
+      const result = generateFactoryTemplate({
+        componentName,
+        componentType: "controller",
+      });
+      expect(result).toStrictEqual(expected);
+    });
+
+    it("should throw a error if a invalid componentType has been passed", () => {
+      const expected = "Invalid component type";
+
+      expect(() =>
+        generateFactoryTemplate({
+          componentName,
+          componentType: <any>"test",
+        })
+      ).toThrowError(expected);
+    });
   });
 });
