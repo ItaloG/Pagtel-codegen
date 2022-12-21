88558d4aa0520bd9d91ef630cb080e7257ff2de3
Italo Gabriel <italo.pereira@pagtel.com.br>
2022-12-21 09:26:47 -0300

    test: ensure file class methods

---
+++ test/unit/file.spec.ts
@@ -0,0 +1,34 @@
+import fsPromises from "node:fs/promises";
+import fs from "node:fs";
+import { File } from "@/file";
+
+describe("#File", () => {
+  test("#verifyExists should return true if folder exists", async () => {
+    jest.spyOn(fs, "existsSync").mockReturnValue(true);
+
+    const result = File.verifyExists({ file: "" });
+
+    expect(fs.existsSync).toHaveBeenCalledTimes(1);
+    expect(result).toStrictEqual(true);
+  });
+
+  test("#verifyExists should return false if folder not exists", async () => {
+    jest.spyOn(fs, "existsSync").mockReturnValue(false);
+
+    const result = File.verifyExists({ file: "" });
+
+    expect(fs.existsSync).toHaveBeenCalledTimes(1);
+    expect(result).toStrictEqual(false);
+  });
+
+  test("#create should create a folder", async () => {
+    jest.spyOn(fsPromises, "writeFile").mockResolvedValue(Promise.resolve());
+
+    await File.create({
+      fileContent: "",
+      fileName: "newFile",
+    });
+
+    expect(fsPromises.writeFile).toHaveBeenCalledTimes(1);
+  });
+});
