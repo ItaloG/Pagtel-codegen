8a196c013d04a80727d3c160ba48dc7e4d0565cb
Italo Gabriel <italo.pereira@pagtel.com.br>
2023-01-03 09:47:45 -0300

    test: ensure validateHasWord function

--- test/unit/utils/validate-string.spec.ts
+++ test/unit/utils/validate-string.spec.ts
@@ -1,17 +1,33 @@
 import { ValidateString } from "@/utils";
 
 describe("#ValidateString", () => {
-  test("#validateSpecialCharacter should return true if string don't have special character", () => {
+  test("#validateSpecialCharacter should return true if string don't has special character", () => {
     const data = "Hello";
     const expected = true;
     const result = ValidateString.validateSpecialCharacter(data);
     expect(result).toEqual(expected);
   });
 
-  test("#validateSpecialCharacter should return false if string have special character", () => {
+  test("#validateSpecialCharacter should return false if string has special character", () => {
     const data = "H1el4lo!";
     const expected = false;
     const result = ValidateString.validateSpecialCharacter(data);
+    expect(result).toEqual(expected);
+  });
+
+  test("#validateHasWord should return true if string has the matchWord", () => {
+    const data = "Hello World";
+    const matchWord = "World";
+    const expected = true;
+    const result = ValidateString.validateHasWord(data, matchWord);
+    expect(result).toEqual(expected);
+  });
+
+  test("#validateHasWord should return false if string don't has the matchWord", () => {
+    const data = "Hello World";
+    const matchWord = "Word";
+    const expected = false;
+    const result = ValidateString.validateHasWord(data, matchWord);
     expect(result).toEqual(expected);
   });
 });
