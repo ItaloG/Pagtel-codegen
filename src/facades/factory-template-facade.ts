cfa948f674ccdff3e2280048892d156c30247f3e
Italo Gabriel <italo.pereira@pagtel.com.br>
2023-01-10 15:54:51 -0300

    fix: add test environment

--- src/facades/factory-template-facade.ts
+++ src/facades/factory-template-facade.ts
@@ -1,7 +1,7 @@
 import { generateFactoryTemplate } from "@/templates";
 import { Folder } from "@/folder";
 import { File } from "@/file";
-import { FormatString } from "@/utils";
+import { FormatString, TESTS_TEMPLATE_PATH } from "@/utils";
 
 export async function generateFactoryFacade({
   name,
@@ -10,10 +10,16 @@ export async function generateFactoryFacade({
 }: FactoryFacade.Params): FactoryFacade.Result {
   if (!factoryType) throw new Error("Você deve informar o tipo da factory");
 
-  const FACTORY_MAIN_PATH = `src/main/factories/${factoryType}`;
+  const env = process.env.NODE_ENV;
+  const FACTORY_MAIN_PATH =
+    env === "test"
+      ? `${TESTS_TEMPLATE_PATH}/main/factories/${factoryType}`
+      : `src/main/factories/${factoryType}`;
 
   const formattedScope = FormatString.convertToKebabCase(scope);
-  const templateName = `make-${FormatString.convertToKebabCase(name)}`;
+  const templateName = `make-${FormatString.convertToKebabCase(
+    name
+  )}-${factoryType}`;
   const templateFilePath = `${FACTORY_MAIN_PATH}/${formattedScope}/${templateName}.ts`;
 
   const templateFileExists = File.verifyExists({ file: templateFilePath });
@@ -24,12 +30,15 @@ export async function generateFactoryFacade({
     folder: `${FACTORY_MAIN_PATH}/${formattedScope}`,
   });
   if (!scopeFolderExists)
-    await Folder.create({ mainPath: FACTORY_MAIN_PATH, newFolder: scope });
+    await Folder.create({
+      mainPath: FACTORY_MAIN_PATH,
+      newFolder: formattedScope,
+    });
 
-  const indexFilePath = `${FACTORY_MAIN_PATH}/index.ts`;
+  const indexFilePath = `${FACTORY_MAIN_PATH}/${formattedScope}/index.ts`;
   await File.create({
     filePath: indexFilePath,
-    fileContent: `export * from ./${templateName}`,
+    fileContent: `export * from './${templateName}'`,
   });
 
   const { template } = generateFactoryTemplate({
