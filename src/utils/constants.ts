7d5e81b9b0708bc71b70a59d6286f7c84021f64b
Italo Gabriel <italo.pereira@pagtel.com.br>
2023-01-11 16:08:30 -0300

    feat: add constant path generator

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,7 @@
-export const TESTS_TEMPLATE_PATH = "test/integration/temp/src";
+export const TEST_TEMPLATE_DEFAULT_PATH = "test/integration/temp/src";
+
+export const generateFactoryPath = (type: string): string => {
+  return process.env.NODE_ENV === "test"
+    ? `${TEST_TEMPLATE_DEFAULT_PATH}/main/factories/${type}`
+    : `src/main/factories/${type}s`;
+};
