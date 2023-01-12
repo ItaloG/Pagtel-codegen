ff5ea84e016c92408e6bce619baca0cadc8fae0a
Italo Gabriel <italo.pereira@pagtel.com.br>
2023-01-12 09:21:09 -0300

    test: ensure middleware path generation

--- test/unit/utils/constants.spec.ts
+++ test/unit/utils/constants.spec.ts
@@ -1,7 +1,7 @@
-import { generateFactoryPath } from "@/utils";
+import { generateFactoryPath, generateMiddlewarePath } from "@/utils";
 
 describe("#Constants", () => {
-  afterAll(() => {
+  afterEach(() => {
     process.env.NODE_ENV = "test";
   });
 
@@ -15,6 +15,19 @@ describe("#Constants", () => {
     process.env.NODE_ENV = "dev";
     const expected = "src/main/factories/any_types";
     const result = generateFactoryPath("any_type");
+    expect(result).toStrictEqual(expected);
+  });
+
+  test('#generateMiddlewarePath should return a "test path" if environment is test', () => {
+    const expected = "test/integration/temp/src/presentation/middlewares";
+    const result = generateMiddlewarePath();
+    expect(result).toStrictEqual(expected);
+  });
+
+  test('#generateMiddlewarePath should return a "production path" if environment is production', () => {
+    process.env.NODE_ENV = "dev";
+    const expected = "src/presentation/middlewares";
+    const result = generateMiddlewarePath();
     expect(result).toStrictEqual(expected);
   });
 });
