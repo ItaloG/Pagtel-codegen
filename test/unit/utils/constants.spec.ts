cca7b94d81cd1792a1a6d0206b190eb416f1d4f1
Italo Gabriel <italo.pereira@pagtel.com.br>
2023-01-14 13:39:36 -0300

    test: ensure usecase path generation

--- test/unit/utils/constants.spec.ts
+++ test/unit/utils/constants.spec.ts
@@ -1,33 +1,56 @@
-import { generateFactoryPath, generateMiddlewarePath } from "@/utils";
+import {
+  generateFactoryPath,
+  generateMiddlewarePath,
+  generateUsecasePath,
+} from "@/utils";
 
 describe("#Constants", () => {
   afterEach(() => {
     process.env.NODE_ENV = "test";
   });
 
-  test('#generateFactoryPath should return a "test path" if environment is test', () => {
-    const expected = "test/integration/temp/src/main/factories/any_type";
-    const result = generateFactoryPath("any_type");
-    expect(result).toStrictEqual(expected);
+  describe("#generateFactoryPath ", () => {
+    it('should return a "test path" if environment is test', () => {
+      const expected = "test/integration/temp/src/main/factories/any_type";
+      const result = generateFactoryPath("any_type");
+      expect(result).toStrictEqual(expected);
+    });
+
+    it('should return a "production path" if environment is production', () => {
+      process.env.NODE_ENV = "dev";
+      const expected = "src/main/factories/any_types";
+      const result = generateFactoryPath("any_type");
+      expect(result).toStrictEqual(expected);
+    });
   });
 
-  test('#generateFactoryPath should return a "production path" if environment is production', () => {
-    process.env.NODE_ENV = "dev";
-    const expected = "src/main/factories/any_types";
-    const result = generateFactoryPath("any_type");
-    expect(result).toStrictEqual(expected);
+  describe("#generateMiddlewarePath", () => {
+    it('should return a "test path" if environment is test', () => {
+      const expected = "test/integration/temp/src/presentation/middlewares";
+      const result = generateMiddlewarePath();
+      expect(result).toStrictEqual(expected);
+    });
+
+    it('should return a "production path" if environment is production', () => {
+      process.env.NODE_ENV = "dev";
+      const expected = "src/presentation/middlewares";
+      const result = generateMiddlewarePath();
+      expect(result).toStrictEqual(expected);
+    });
   });
 
-  test('#generateMiddlewarePath should return a "test path" if environment is test', () => {
-    const expected = "test/integration/temp/src/presentation/middlewares";
-    const result = generateMiddlewarePath();
-    expect(result).toStrictEqual(expected);
-  });
+  describe("#generateUsecasePath", () => {
+    it('should return a "test path" if environment is test', () => {
+      const expected = "test/integration/temp/src/data/usecases/any_type";
+      const result = generateUsecasePath("any_type");
+      expect(result).toStrictEqual(expected);
+    });
 
-  test('#generateMiddlewarePath should return a "production path" if environment is production', () => {
-    process.env.NODE_ENV = "dev";
-    const expected = "src/presentation/middlewares";
-    const result = generateMiddlewarePath();
-    expect(result).toStrictEqual(expected);
+    it('should return a "production path" if environment is production', () => {
+      process.env.NODE_ENV = "dev";
+      const expected = "src/data/usecases/any_type";
+      const result = generateUsecasePath("any_type");
+      expect(result).toStrictEqual(expected);
+    });
   });
 });
