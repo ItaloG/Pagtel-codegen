9fe929438f837de501a0c48555cec9a7274f0933
Italo Gabriel <italo.pereira@pagtel.com.br>
2023-01-19 10:45:25 -0300

    feat: add service generate facade

--- src/facades/index.ts
+++ src/facades/index.ts
@@ -2,3 +2,4 @@ export * from "./factory-template-facade";
 export * from "./middleware-template-facade";
 export * from "./usecase-template-facade";
 export * from "./repository-template-facade";
+export * from "./service-template-facade";
---
+++ src/facades/service-template-facade.ts
@@ -0,0 +1,49 @@
+import { File } from "@/file";
+import { Folder } from "@/folder";
+import { generateServiceTemplate } from "@/templates";
+import { FormatString, generateServicePath } from "@/utils";
+import { generateIndex } from "./utils";
+
+export async function generateServiceFacade({
+  name,
+  scope,
+}: ServiceFacade.Params): ServiceFacade.Result {
+  const SERVICE_MAIN_PATH = generateServicePath();
+  const FORMATTED_SCOPE = FormatString.convertToKebabCase(scope);
+  const TEMPLATE_NAME = FormatString.convertToKebabCase(name);
+  const TEMPLATE_FILE_PATH = `${SERVICE_MAIN_PATH}/${FORMATTED_SCOPE}/${TEMPLATE_NAME}.ts`;
+
+  const templateFileExists = File.verifyExists({ file: TEMPLATE_FILE_PATH });
+  if (templateFileExists)
+    return { type: "info", message: "Service file already exists" };
+
+  const scopeFolderExists = Folder.verifyExists({
+    folder: `${SERVICE_MAIN_PATH}/${FORMATTED_SCOPE}`,
+  });
+  if (!scopeFolderExists)
+    await Folder.create({
+      mainPath: SERVICE_MAIN_PATH,
+      newFolder: FORMATTED_SCOPE,
+    });
+
+  await generateIndex(
+    `${SERVICE_MAIN_PATH}/${FORMATTED_SCOPE}/index.ts`,
+    TEMPLATE_NAME
+  );
+
+  const { template } = generateServiceTemplate({
+    componentName: name,
+  });
+
+  await File.create({ filePath: TEMPLATE_FILE_PATH, fileContent: template });
+
+  return { type: "success", message: "service generated" };
+}
+
+namespace ServiceFacade {
+  export type Params = {
+    name: string;
+    scope: string;
+  };
+  export type Result = Promise<{ type: string; message: string }>;
+}
