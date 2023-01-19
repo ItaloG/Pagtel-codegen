9dba55fc199b5910745b750cac636145beb25b4c
Italo Gabriel <italo.pereira@pagtel.com.br>
2023-01-19 14:04:05 -0300

    test: ensure data protocol template facade

---
+++ test/integration/data-protocol-template-facade.spec.ts
@@ -0,0 +1,54 @@
+import fsPromises from "node:fs/promises";
+import fs from "node:fs";
+import { generateDataProtocolPath } from "@/utils";
+import { generateDataProtocolFacade } from "@/facades";
+
+describe("#Integration Data Protocol Facade", () => {
+  afterEach(async () => {
+    await fsPromises.rm(`${__dirname}/temp/src`, {
+      recursive: true,
+    });
+  });
+
+  it("should generate a db data protocol", async () => {
+    const data = { name: "GetDog", scope: "Dog" };
+    const type = "db";
+    await generateDataProtocolFacade({
+      ...data,
+      protocolType: type,
+    });
+
+    const MAIN_PATH = generateDataProtocolPath(type);
+
+    const templatePath = `${MAIN_PATH}/${data.scope.toLowerCase()}/get-dog-repository.ts`;
+    const indexPath = `${MAIN_PATH}/${data.scope.toLowerCase()}/index.ts`;
+
+    const templateResult = fs.existsSync(templatePath);
+    const indexResult = fs.existsSync(indexPath);
+    const expected = true;
+
+    expect(templateResult).toStrictEqual(expected);
+    expect(indexResult).toStrictEqual(expected);
+  });
+
+  it("should generate a controller factory", async () => {
+    const data = { name: "GetDog", scope: "Dog" };
+    const type = "http";
+    await generateDataProtocolFacade({
+      ...data,
+      protocolType: type,
+    });
+
+    const MAIN_PATH = generateDataProtocolPath(type);
+
+    const templatePath = `${MAIN_PATH}/${data.scope.toLowerCase()}/get-dog-service.ts`;
+    const indexPath = `${MAIN_PATH}/${data.scope.toLowerCase()}/index.ts`;
+
+    const templateResult = fs.existsSync(templatePath);
+    const indexResult = fs.existsSync(indexPath);
+    const expected = true;
+
+    expect(templateResult).toStrictEqual(expected);
+    expect(indexResult).toStrictEqual(expected);
+  });
+});
---
+++ test/unit/facades/data-protocol-template-facade.spec.ts
@@ -0,0 +1,81 @@
+import { generateDataProtocolFacade } from "@/facades";
+import { File } from "@/file";
+import { Folder } from "@/folder";
+
+describe("#Data Protocol Facade", () => {
+  beforeAll(() => {
+    jest.spyOn(Folder, "create").mockResolvedValue(Promise.resolve());
+    jest.spyOn(File, "create").mockResolvedValue(Promise.resolve());
+  });
+
+  afterAll(() => {
+    jest.restoreAllMocks();
+  });
+
+  it('should throw a error if "factoryType" is undefined', async () => {
+    const data = { name: "any_name", scope: "any_scope" };
+    const promise = generateDataProtocolFacade(data);
+    await expect(promise).rejects.toThrowError(
+      "Você deve informar o tipo de protocol"
+    );
+  });
+
+  it("should return a info if the template file already exits", async () => {
+    jest.spyOn(File, "verifyExists").mockReturnValueOnce(true);
+
+    const expected = {
+      type: "info",
+      message: "Data protocol file already exists",
+    };
+    const result = await generateDataProtocolFacade({
+      name: "any_name",
+      scope: "any_scope",
+      protocolType: "db",
+    });
+    expect(result).toStrictEqual(expected);
+  });
+
+  it("should create a folder if it not exists", async () => {
+    jest.spyOn(File, "verifyExists").mockReturnValueOnce(false);
+    jest.spyOn(Folder, "verifyExists").mockReturnValueOnce(false);
+
+    const createFolderSpy = jest.spyOn(Folder, "create");
+
+    await generateDataProtocolFacade({
+      name: "any_name",
+      scope: "any_scope",
+      protocolType: "db",
+    });
+    expect(createFolderSpy).toHaveBeenCalled();
+  });
+
+  it("should not create a folder if it not exists", async () => {
+    jest.spyOn(Folder, "verifyExists").mockReturnValueOnce(true);
+
+    const createFolderSpy = jest.spyOn(Folder, "create");
+
+    await generateDataProtocolFacade({
+      name: "any_name",
+      scope: "any_scope",
+      protocolType: "db",
+    });
+    expect(createFolderSpy).not.toHaveBeenCalled();
+  });
+
+  it("should create a data protocol template file", async () => {
+    jest.spyOn(File, "verifyExists").mockReturnValueOnce(false);
+    jest.spyOn(Folder, "verifyExists").mockReturnValueOnce(true);
+
+    const fileCreateSpy = jest.spyOn(File, "create");
+
+    const expected = { type: "success", message: "data protocol generated" };
+    const result = await generateDataProtocolFacade({
+      name: "any_name",
+      scope: "any_scope",
+      protocolType: "db",
+    });
+
+    expect(fileCreateSpy).toHaveBeenCalledTimes(2);
+    expect(result).toStrictEqual(expected);
+  });
+});
