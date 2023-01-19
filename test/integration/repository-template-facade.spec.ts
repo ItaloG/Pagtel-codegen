d53da850f0aed6e49a0c0420336e719057d29327
Italo Gabriel <italo.pereira@pagtel.com.br>
2023-01-19 09:28:46 -0300

    test: ensure repository generation facade

---
+++ test/integration/repository-template-facade.spec.ts
@@ -0,0 +1,56 @@
+import fsPromises from "node:fs/promises";
+import fs from "node:fs";
+import { generateRepositoryFacade } from "@/facades";
+import { FormatString, generateRepositoryPath } from "@/utils";
+
+describe("#Integration Repository Facade", () => {
+  afterEach(async () => {
+    await fsPromises.rm(`${__dirname}/temp/src`, {
+      recursive: true,
+    });
+  });
+
+  it("should generate a mssql repository", async () => {
+    const data = { name: "GetDog", schema: "Dog", database: "Pets" };
+    const type = "mssql";
+    await generateRepositoryFacade({
+      ...data,
+      repositoryType: type,
+    });
+
+    const MAIN_PATH = generateRepositoryPath(type);
+
+    const templatePath = `${MAIN_PATH}/${data.database.toLowerCase()}/${data.schema.toLowerCase()}/get-dog-repository.ts`;
+    const indexPath = `${MAIN_PATH}/${data.database.toLowerCase()}/${data.schema.toLowerCase()}/index.ts`;
+
+    const templateResult = fs.existsSync(templatePath);
+    const indexResult = fs.existsSync(indexPath);
+    const expected = true;
+
+    expect(templateResult).toStrictEqual(expected);
+    expect(indexResult).toStrictEqual(expected);
+  });
+
+  it("should generate a mongodb repository", async () => {
+    const data = { name: "GetDog", schema: "Dog", database: "Pets" };
+    const type = "mongodb";
+    await generateRepositoryFacade({
+      ...data,
+      repositoryType: type,
+    });
+
+    const MAIN_PATH = generateRepositoryPath(type);
+
+    const formattedName = FormatString.convertToKebabCase(data.name);
+
+    const templatePath = `${MAIN_PATH}/${formattedName}/get-dog-repository.ts`;
+    const indexPath = `${MAIN_PATH}/${formattedName}/index.ts`;
+
+    const templateResult = fs.existsSync(templatePath);
+    const indexResult = fs.existsSync(indexPath);
+    const expected = true;
+
+    expect(templateResult).toStrictEqual(expected);
+    expect(indexResult).toStrictEqual(expected);
+  });
+});
---
+++ test/unit/facades/repository-template-facade.spec.ts
@@ -0,0 +1,105 @@
+import { generateRepositoryFacade } from "@/facades";
+import { File } from "@/file";
+import { Folder } from "@/folder";
+
+describe("#Repository Facade", () => {
+  beforeAll(() => {
+    jest.spyOn(Folder, "create").mockResolvedValue(Promise.resolve());
+    jest.spyOn(File, "create").mockResolvedValue(Promise.resolve());
+  });
+
+  afterAll(() => {
+    jest.restoreAllMocks();
+  });
+
+  it('should throw a error if "repositoryType" is undefined', async () => {
+    const data = {
+      name: "any_name",
+      database: "any_database",
+      schema: "any_schema",
+    };
+    const promise = generateRepositoryFacade(data);
+    await expect(promise).rejects.toThrowError(
+      "Você deve informar o tipo do repository"
+    );
+  });
+
+  it("should return a info if the template file already exits", async () => {
+    jest.spyOn(File, "verifyExists").mockReturnValueOnce(true);
+
+    const expected = {
+      type: "info",
+      message: "Repository file already exists",
+    };
+    const result = await generateRepositoryFacade({
+      name: "any_name",
+      database: "any_database",
+      schema: "any_schema",
+      repositoryType: "mongodb",
+    });
+    expect(result).toStrictEqual(expected);
+  });
+
+  it("if 'repositoryType' is mssql should create a database and schema folder if it not exists", async () => {
+    jest.spyOn(File, "verifyExists").mockReturnValueOnce(false);
+    jest.spyOn(Folder, "verifyExists").mockReturnValueOnce(false);
+
+    const createFolderSpy = jest.spyOn(Folder, "create");
+
+    await generateRepositoryFacade({
+      name: "any_name",
+      database: "any_database",
+      schema: "any_schema",
+      repositoryType: "mssql",
+    });
+    expect(createFolderSpy).toHaveBeenCalledTimes(2);
+  });
+
+  it("if 'repositoryType' is mongodb should create a folder if it not exists", async () => {
+    jest.spyOn(File, "verifyExists").mockReturnValueOnce(false);
+    jest.spyOn(Folder, "verifyExists").mockReturnValueOnce(false);
+
+    const createFolderSpy = jest.spyOn(Folder, "create");
+
+    await generateRepositoryFacade({
+      name: "any_name",
+      database: "any_database",
+      schema: "any_schema",
+      repositoryType: "mongodb",
+    });
+    expect(createFolderSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it("should not create a folder if it not exists", async () => {
+    jest.spyOn(Folder, "verifyExists").mockReturnValueOnce(true);
+    jest.spyOn(Folder, "verifyExists").mockReturnValueOnce(true);
+
+    const createFolderSpy = jest.spyOn(Folder, "create");
+
+    await generateRepositoryFacade({
+      name: "any_name",
+      database: "any_database",
+      schema: "any_schema",
+      repositoryType: "mssql",
+    });
+    expect(createFolderSpy).not.toHaveBeenCalled();
+  });
+
+  it("should create a repository template file", async () => {
+    jest.spyOn(File, "verifyExists").mockReturnValueOnce(false);
+    jest.spyOn(Folder, "verifyExists").mockReturnValueOnce(true);
+
+    const fileCreateSpy = jest.spyOn(File, "create");
+
+    const expected = { type: "success", message: "repository generated" };
+    const result = await generateRepositoryFacade({
+      name: "any_name",
+      database: "any_database",
+      schema: "any_schema",
+      repositoryType: "mssql",
+    });
+
+    expect(fileCreateSpy).toHaveBeenCalledTimes(2);
+    expect(result).toStrictEqual(expected);
+  });
+});
