769a29e21609ed9f432368fc3713ff189e6614f0
Italo Gabriel <italo.pereira@pagtel.com.br>
2023-01-19 13:14:36 -0300

    test: ensure controller facade generation

---
+++ test/integration/controller-template-facade.spec.ts
@@ -0,0 +1,31 @@
+import fsPromises from "node:fs/promises";
+import fs from "node:fs";
+import { generateControllerFacade } from "@/facades";
+import { generateControllerPath } from "@/utils";
+
+describe("#Integration Controller Facade", () => {
+  afterEach(async () => {
+    await fsPromises.rm(`${__dirname}/temp/src`, {
+      recursive: true,
+    });
+  });
+
+  it("should generate a middleware", async () => {
+    const data = { name: "GetDog", scope: "Dog" };
+    await generateControllerFacade({
+      ...data,
+    });
+
+    const MAIN_PATH = generateControllerPath();
+
+    const templatePath = `${MAIN_PATH}/${data.scope.toLowerCase()}/get-dog-controller.ts`;
+    const indexPath = `${MAIN_PATH}/${data.scope.toLowerCase()}/index.ts`;
+
+    const templateResult = fs.existsSync(templatePath);
+    const indexResult = fs.existsSync(indexPath);
+    const expected = true;
+
+    expect(templateResult).toStrictEqual(expected);
+    expect(indexResult).toStrictEqual(expected);
+  });
+});
