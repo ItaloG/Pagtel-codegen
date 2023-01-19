95cf3f6d96b6ccb4ddce7eee41f8df279e9334ab
Italo Gabriel <italo.pereira@pagtel.com.br>
2023-01-19 15:10:37 -0300

    test: ensure domain usecase facade generator

---
+++ test/integration/domain-usecase-template-facade.spec.ts
@@ -0,0 +1,31 @@
+import fsPromises from "node:fs/promises";
+import fs from "node:fs";
+import { generateDomainUsecaseFacade } from "@/facades";
+import { generateDomainUsecasePath } from "@/utils";
+
+describe("#Integration Domain Usecase Facade", () => {
+  afterEach(async () => {
+    await fsPromises.rm(`${__dirname}/temp/src`, {
+      recursive: true,
+    });
+  });
+
+  it("should generate a middleware", async () => {
+    const data = { name: "GetDog", scope: "Dog" };
+    await generateDomainUsecaseFacade({
+      ...data,
+    });
+
+    const MAIN_PATH = generateDomainUsecasePath();
+
+    const templatePath = `${MAIN_PATH}/${data.scope.toLowerCase()}/get-dog.ts`;
+    const indexPath = `${MAIN_PATH}/${data.scope.toLowerCase()}/index.ts`;
+
+    const templateResult = fs.existsSync(templatePath);
+    const indexResult = fs.existsSync(indexPath);
+    const expected = true;
+
+    expect(templateResult).toStrictEqual(expected);
+    expect(indexResult).toStrictEqual(expected);
+  });
+});
