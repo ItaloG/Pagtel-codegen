3230ec68fad79a2ee72c6db20e8993ea7e2b1c52
Italo Gabriel <italo.pereira@pagtel.com.br>
2023-01-19 09:28:23 -0300

    feat: add repository facade

--- src/facades/index.ts
+++ src/facades/index.ts
@@ -1,3 +1,4 @@
 export * from "./factory-template-facade";
 export * from "./middleware-template-facade";
 export * from "./usecase-template-facade";
+export * from "./repository-template-facade";
---
+++ src/facades/repository-template-facade.ts
@@ -0,0 +1,75 @@
+import { File } from "@/file";
+import { generateRepositoryTemplate } from "@/templates";
+import { FormatString, generateRepositoryPath } from "@/utils";
+import {
+  generateIndex,
+  validateMongodbFolder,
+  validateMssqlFolder,
+} from "./utils";
+
+export async function generateRepositoryFacade({
+  name,
+  database,
+  repositoryType,
+  schema,
+}: RepositoryFacade.Params): RepositoryFacade.Result {
+  if (!repositoryType)
+    throw new Error("Você deve informar o tipo do repository");
+
+  const REPOSITORY_MAIN_PATH = generateRepositoryPath(repositoryType);
+  const TEMPLATE_NAME = `${FormatString.convertToKebabCase(name)}-repository`;
+  const SCOPE_NAME = FormatString.convertToKebabCase(name);
+  const FORMATTED_SCHEMA_NAME = `${FormatString.convertToKebabCase(schema)}`;
+  const FORMATTED_DATABASE_NAME = `${FormatString.convertToKebabCase(
+    database
+  )}`;
+
+  const TEMPLATE_FILE_PATH =
+    repositoryType === "mssql"
+      ? `${REPOSITORY_MAIN_PATH}/${FORMATTED_DATABASE_NAME}/${FORMATTED_SCHEMA_NAME}`
+      : `${REPOSITORY_MAIN_PATH}/${SCOPE_NAME}`;
+
+  const templateFileExists = File.verifyExists({
+    file: `${TEMPLATE_FILE_PATH}/${TEMPLATE_NAME}.ts`,
+  });
+  if (templateFileExists)
+    return { type: "info", message: "Repository file already exists" };
+
+  const folderValidations = {
+    mongodb: validateMongodbFolder,
+    mssql: validateMssqlFolder,
+  };
+
+  const data = {
+    mainPath: REPOSITORY_MAIN_PATH,
+    database: FORMATTED_DATABASE_NAME,
+    schema: FORMATTED_SCHEMA_NAME,
+    scope: SCOPE_NAME,
+  };
+
+  await folderValidations[repositoryType]({ ...data });
+
+  await generateIndex(`${TEMPLATE_FILE_PATH}/index.ts`, TEMPLATE_NAME);
+
+  const { template } = generateRepositoryTemplate({
+    componentName: name,
+    componentType: repositoryType,
+  });
+
+  await File.create({
+    filePath: `${TEMPLATE_FILE_PATH}/${TEMPLATE_NAME}.ts`,
+    fileContent: template,
+  });
+
+  return { type: "success", message: "repository generated" };
+}
+
+namespace RepositoryFacade {
+  export type Params = {
+    name: string;
+    repositoryType?: "mongodb" | "mssql";
+    database: string;
+    schema: string;
+  };
+  export type Result = Promise<{ type: string; message: string }>;
+}
